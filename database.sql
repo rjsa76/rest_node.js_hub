0f3626a3a92196a22a63effd0fb504336fcc39c6
rjsa76 <39095668+rjsa76@users.noreply.github.com>
2018-05-21 12:37:17 +0100

    Update database.sql
    add comments

--- database.sql
+++ database.sql
@@ -1,8 +1,10 @@
+-- Tabela para os Albuns
 CREATE TABLE album (
   id serial primary key,
   description varchar(255)
 );
 
+-- Tabela para as Fotos
 CREATE TABLE photo (
   id serial primary key,
   album_id integer,
